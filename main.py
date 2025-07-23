f3ac15670f0081db713642914a91c3bca0b7757d
pneumatech100 <j.white@pneumagroup.co.uk>
2025-07-23 21:46:00 +0100

    Final

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ def get_book_text(path):
 from stats import string_word_count
 from stats import string_character_count
 from stats import report
+import sys
 
 def main():
-    path ="books/frankenstein.txt"
+   # path ="books/franken(stein.txt"
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)  
+    path = sys.argv[1] 
     book_text = get_book_text(path)
     words =string_word_count(book_text)
     chardict = string_character_count(book_text)
@@ -16,7 +22,7 @@ def main():
     #print(chardict)
     
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}")
     print(f"Found {words} total words")
     print("--------- Character Count -------")
     rep = report(book_text)
